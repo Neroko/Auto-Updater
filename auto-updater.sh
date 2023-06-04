7b5de118af8005b3edc2d64c438289f146bb7658
TJ <tj.anscombe@gmail.com>
2023-06-04 11:46:12 -0400

    Update auto-updater.sh

--- auto-updater.sh
+++ auto-updater.sh
@@ -5,22 +5,6 @@ download_url="https://raw.githubusercontent.com/Neroko/Auto-Updater/main/auto-up
 file_name="auto-updater.sh"
 
 echo "Auto Updater (Version "$current_version")"
-#sudo apt update
-#sudo apt upgrade -y
-#sudo apt full-upgrade -y
-#sudo apt dist-upgrade -y
-#sudo apt autoclean
-#sudo apt autoremove
-
-# Downloading small Google Drive Files using WGET (less than 100MB)
-# Change 'FILEID' and 'FILENAME'
-#wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O FILENAME
-
-# Downloading large Google rive Files using WGET (over 100MB)
-# Change 'FILEID' and 'FILENAME'
-#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O FILENAME && rm -rf /tmp/cookies.txt
-
-#wget --no-check-certificate "https://drive.google.com/uc?export=download&id="$file_id"" -O "$file_name"
 
 cp "$file_name" ""$filename".old"
 
